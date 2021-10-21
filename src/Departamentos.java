5f41c0f182a18baac0f5a2f7459729b92f402155
Your Name <you@example.com>
2021-10-21 21:22:38 +0200

    messages

--- src/Departamentos.java
+++ src/Departamentos.java
@@ -1,10 +1,13 @@
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.SQLTimeoutException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Departamentos {
     private ArrayList<Departamento> departamentos;
+    private final String TABLE = "DEPT";
 
     public Departamentos() {
         Conectar conexion = new Conectar();
@@ -34,7 +37,51 @@ public class Departamentos {
         return departamentos;
     }
 
-    public boolean insertDept(){
-        
+    public boolean insertDept(String deptname) throws SQLException, SQLTimeoutException {
+        Conectar conexion = new Conectar();
+        Random random = new Random();
+        int deptno = random.nextInt(60) + 40;
+        this.departamentos.add(new Departamento(deptno, deptname, "A CORUÑA"));
+        Statement s = conexion.getConect().createStatement();
+        String sql = "INSERT INTO " + this.TABLE + "VALUES (" + Integer.toString(deptno) + ", '" + deptname
+                + "', 'A CORUÑA');";
+        int rowCount = s.executeUpdate(sql);
+        conexion.close();
+        if (rowCount == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int deleteDept(String nombre) throws SQLException, IndexOutOfBoundsException {
+        Conectar conexion = new Conectar();
+        Statement statement = conexion.getConect().createStatement();
+        String sql = "DELETE FROM " + TABLE + " WHERE ENAME = '" + nombre + "';";
+        for (Departamento departamento : departamentos) {
+            if (departamento.getDname().equals(nombre)) {
+                if (!departamentos.remove(departamento))
+                    throw new IndexOutOfBoundsException();
+                else
+                    break;
+            }
+        }
+        int r = statement.executeUpdate(sql);
+        conexion.close();
+        return r;
+    }
+
+    public int updateDept(String oldName, String newName) throws SQLException {
+        Conectar conexion = new Conectar();
+        Statement statement = conexion.getConect().createStatement();
+        String sql = "UPDATE " + TABLE + " SET NOMBRE = " + newName + " WHERE ENAME = '" + oldName + "';";
+        for (Departamento departamento : departamentos) {
+            if (departamento.getDname().equals(oldName)) {
+                departamento.setDname(newName);
+            }
+        }
+        int r = statement.executeUpdate(sql);
+        conexion.close();
+        return r;
     }
 }
