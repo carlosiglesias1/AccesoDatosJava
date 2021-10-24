0727d98155d6a042fa1eb1b034cfb8d47e54f5cd
carlosiglesias1 <carlosig490@gmail.com>
2021-10-25 01:41:59 +0200

    cositas

--- src/Conectar.java
+++ src/Conectar.java
@@ -3,33 +3,34 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conectar {
-    private Connection conect;
+    private static final Conectar CONNECTION = new Conectar();
+    private Connection conexion;
 
-    public Conectar() {
+    private Conectar() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             // variables
             final String url = "jdbc:mysql:///carlos";
             final String user = "root";
-            final String password ="YourPass";
+            final String password = "YourPass";
             // establish the connection
 
-            this.conect = DriverManager.getConnection(url, user, password);
+            this.conexion = DriverManager.getConnection(url, user, password);
         } catch (SQLException | ClassNotFoundException ex) {
             System.out.println(ex.getStackTrace());
         }
     }
 
-    public Connection getConect() {
-        return conect;
+    public static Connection getConect() {
+        return CONNECTION.conexion;
     }
 
     public void close() {
         try {
-            this.conect.close();    
+            this.CONNECTION.conexion.close();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        
+
     }
 }
--- src/Departamentos.java
+++ src/Departamentos.java
@@ -1,25 +1,26 @@
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.SQLTimeoutException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Departamentos {
-    private ArrayList<Departamento> departamentos;
-    private final String TABLE = "DEPT";
+    private ArrayList<Departamento> deptList;
+    private static final String TABLE = "DEPT";
 
     public Departamentos() {
-        Conectar conexion = new Conectar();
-        this.departamentos = new ArrayList<Departamento>();
+        Connection conexion = Conectar.getConect();
+        this.deptList = new ArrayList<>();
         try {
-            Statement s = conexion.getConect().createStatement();
+            Statement s = conexion.createStatement();
             String sql = "CREATE TABLE IF NOT EXISTS DEPT (DEPTNO DECIMAL (2,0), DNAME VARCHAR(14), LOC VARCHAR(13));";
             s.execute(sql);
             sql = "SELECT * FROM DEPT;";
             ResultSet resultSet = s.executeQuery(sql);
             while (resultSet.next()) {
-                departamentos
+                deptList
                         .add(new Departamento(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3)));
             }
         } catch (IndexOutOfBoundsException | SQLException e) {
@@ -30,23 +31,21 @@ public class Departamentos {
                 System.out.println(e.getStackTrace());
             }
         }
-        conexion.close();
     }
 
-    public ArrayList<Departamento> getDepartamentos() {
-        return departamentos;
+    public List<Departamento> getDepartamentos() {
+        return deptList;
     }
 
-    public boolean insertDept(String deptname) throws SQLException, SQLTimeoutException {
-        Conectar conexion = new Conectar();
+    public boolean insertDept(String deptname) throws SQLException {
+        Connection conexion = Conectar.getConect();
         Random random = new Random();
         int deptno = random.nextInt(60) + 40;
-        this.departamentos.add(new Departamento(deptno, deptname, "A CORUÑA"));
-        Statement s = conexion.getConect().createStatement();
-        String sql = "INSERT INTO " + this.TABLE + "VALUES (" + Integer.toString(deptno) + ", '" + deptname
+        this.deptList.add(new Departamento(deptno, deptname, "A CORUÑA"));
+        Statement s = conexion.createStatement();
+        String sql = "INSERT INTO " + Departamentos.TABLE + "VALUES (" + Integer.toString(deptno) + ", '" + deptname
                 + "', 'A CORUÑA');";
         int rowCount = s.executeUpdate(sql);
-        conexion.close();
         if (rowCount == 1) {
             return true;
         } else {
@@ -55,33 +54,29 @@ public class Departamentos {
     }
 
     public int deleteDept(String nombre) throws SQLException, IndexOutOfBoundsException {
-        Conectar conexion = new Conectar();
-        Statement statement = conexion.getConect().createStatement();
+        Connection conexion = Conectar.getConect();
+        Statement statement = conexion.createStatement();
         String sql = "DELETE FROM " + TABLE + " WHERE ENAME = '" + nombre + "';";
-        for (Departamento departamento : departamentos) {
+        for (Departamento departamento : deptList) {
             if (departamento.getDname().equals(nombre)) {
-                if (!departamentos.remove(departamento))
+                if (!deptList.remove(departamento))
                     throw new IndexOutOfBoundsException();
                 else
                     break;
             }
         }
-        int r = statement.executeUpdate(sql);
-        conexion.close();
-        return r;
+        return statement.executeUpdate(sql);
     }
 
     public int updateDept(String oldName, String newName) throws SQLException {
-        Conectar conexion = new Conectar();
-        Statement statement = conexion.getConect().createStatement();
+        Connection conexion = Conectar.getConect();
+        Statement statement = conexion.createStatement();
         String sql = "UPDATE " + TABLE + " SET NOMBRE = " + newName + " WHERE ENAME = '" + oldName + "';";
-        for (Departamento departamento : departamentos) {
+        for (Departamento departamento : deptList) {
             if (departamento.getDname().equals(oldName)) {
                 departamento.setDname(newName);
             }
         }
-        int r = statement.executeUpdate(sql);
-        conexion.close();
-        return r;
+        return statement.executeUpdate(sql);
     }
 }
